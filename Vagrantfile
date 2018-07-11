565a1032b65bf8451e02903932c5d9d8ca9d0262
beitaz <beitaz@icloud.com>
2018-07-11 19:09:25 +0800

    修改 vagrantfile 和 provision 脚本以优化界面显示

--- Vagrantfile
+++ Vagrantfile
@@ -49,13 +49,14 @@ Vagrant.configure('2') do |config|
   # backing providers for Vagrant. These expose provider-specific options.
   # Example for VirtualBox:
   #
-  # config.vm.provider "virtualbox" do |vb|
-  #   # Display the VirtualBox GUI when booting the machine
-  #   vb.gui = true
-  #
-  #   # Customize the amount of memory on the VM:
-  #   vb.memory = "1024"
-  # end
+  config.vm.provider 'virtualbox' do |vb|
+    #   # Display the VirtualBox GUI when booting the machine
+    #   vb.gui = true
+    #
+    #   # Customize the amount of memory on the VM:
+    vb.memory = 2048
+    vb.cpus = 1
+  end
   #
   # View the documentation for the provider you are using for more
   # information on available options.
@@ -68,4 +69,13 @@ Vagrant.configure('2') do |config|
   #   yum install -y apache2
   # SHELL
   config.vm.provision :shell, path: 'provisions.sh'
+
+  config.trigger.after :up do |trigger|
+    trigger.info = 'Virtual Machine was started.'
+  end
+  # As of version 2.1.0, Vagrant is capable of executing machine triggers before 
+  # or after Vagrant commands.
+  # config.trigger.before [:up, :destroy, :halt, :package] do |trigger|
+  #   ...
+  # end
 end
--- provisions.sh
+++ provisions.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 
 yum update
-yum install -y epel-release yum-utils
-yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm
+yum install -y epel-release yum-utils wget 2> /dev/null
+yum install -y http://rpms.remirepo.net/enterprise/remi-release-7.rpm 2> /dev/null
+
+# 安装 PHP 7.2
 yum-config-manager --enable remi-php72
-yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql
+yum install -y php php-common php-opcache php-mcrypt php-cli php-gd php-curl php-mysql 2> /dev/null
 systemctl start httpd && systemctl enable httpd
-yum -y remove mariadb-libs.x86_64
-rpm -ivh https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm
-yum -y install mysql-community-server --disablerepo=mysql80-community --enablerepo=mysql57-community
+
+# 安装 MySQL 5.7
+yum -y remove mariadb-libs.x86_64 2> /dev/null
+rpm -ivh https://dev.mysql.com/get/mysql80-community-release-el7-1.noarch.rpm 2> /dev/null
+yum -y install mysql-community-server --disablerepo=mysql80-community --enablerepo=mysql57-community 2> /dev/null
 systemctl start mysqld && systemctl enable mysqld
-pwd=`cat /var/log/mysqld.log | grep -i 'temporary password' | cut -d ' ' -f 11`
+
+# 修改 MySQL 密码
+pwd=`cat /var/log/mysqld.log | grep -i 'temporary password' | cut -d ' ' -f 11` # 查询临时密码
 echo "Mysql temporary password is: ${pwd}"
-mysql -uroot -p${pwd} --connect-expired-password -e "SET GLOBAL validate_password_policy = 0; SET PASSWORD = PASSWORD('123abc..'); FLUSH PRIVILEGES;"
-# systemctl stop firewalld
+mysql -uroot -p${pwd} --connect-expired-password -e "SET GLOBAL validate_password_policy = 0; SET PASSWORD = PASSWORD('123abc..'); FLUSH PRIVILEGES;" 2> /dev/null
+
+# 添加 PHP 测试文件
+touch /var/www/html/info.php
+echo '<?php phpinfo(); ?>' > /var/www/html/info.php
+
+# 清理 yum
+yum clean all
+rm -rf /var/cache/yum
+# systemctl stop firewalld # 停止防火墙
